ea9746af9a9d7fc4f671f50d9aa6f5d94f890afd
Matthias Röhricht <gunnerg@web.de>
2024-08-14 11:10:03 +0200

    Improved calculate shooting range

--- src/backend/BotBattle.Engine/Services/TankCalculator.cs
+++ src/backend/BotBattle.Engine/Services/TankCalculator.cs
@@ -4,24 +4,51 @@ using BotBattle.Engine.Models.States;
 
 namespace BotBattle.Engine.Services;
 
-public class TankCalculator
+public static class TankCalculator
 {
-    public static TankAction CalculateNextAction(string payloadHashString, int gridWidth, int gridHeight, int blastRadius)
+    public static TankAction CalculateNextAction(string payloadHashString)
     {
         var direction = payloadHashString[..2];
         var shootingRange = payloadHashString[2..4];
         var actionDecision = payloadHashString[4..5];
 
         var rawShootingRange = int.Parse(shootingRange, NumberStyles.HexNumber);
-        var gridSize = Math.Max(gridWidth, gridHeight);
-        var mappedShootingRange = Math.Max(blastRadius + 1, (int)((double)rawShootingRange / 255 * gridSize));
 
         return new TankAction
         {
             Rotation = MapToDirection(int.Parse(direction, NumberStyles.HexNumber)),
             ShouldShoot = actionDecision[0] % 2 == 0,
-            ShootingRange = mappedShootingRange
+            RawShootingRange = rawShootingRange
         };
+    }
+
+    public static int CalculateShootingRange(int rawShootingRange, int blastRadius, int gridWidth, int gridHeight,
+        Position tankPosition)
+    {
+        var gridSize = tankPosition.Direction switch
+        {
+            Direction.North or Direction.South => gridHeight,
+            Direction.East or Direction.West => gridWidth,
+            Direction.NorthEast or Direction.SouthWest or Direction.SouthEast or Direction.NorthWest => Math.Min(
+                gridWidth, gridHeight),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        var mappedShootingRange = Math.Max(blastRadius + 1, (int)((double)rawShootingRange / 255 * gridSize));
+
+        var maxRange = tankPosition.Direction switch
+        {
+            Direction.North => tankPosition.Y,
+            Direction.NorthEast => Math.Min(tankPosition.Y, gridWidth - tankPosition.X),
+            Direction.East => gridWidth - tankPosition.X,
+            Direction.SouthEast => Math.Min(gridHeight - tankPosition.Y, gridWidth - tankPosition.X),
+            Direction.South => gridHeight - tankPosition.Y,
+            Direction.SouthWest => Math.Min(gridHeight - tankPosition.Y, tankPosition.X),
+            Direction.West => tankPosition.X,
+            Direction.NorthWest => Math.Min(tankPosition.Y, tankPosition.X),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        return Math.Min(mappedShootingRange, maxRange);
     }
 
     private static Direction MapToDirection(int value)
