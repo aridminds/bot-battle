03fc282f74e656d41a49d50e4a9c6ef9c4d75648
dmkk3r <dominik.koerber.95@gmail.com>
2024-08-22 23:14:50 +0200

    First draft of agent template

---
+++ sample/BotBattle.Agent/BotBattle.Agent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using Extism;
+
+namespace BotBattle.Agent;
+
+[JsonSerializable(typeof(object))]
+public partial class SourceGenerationContext : JsonSerializerContext
+{
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        // Note: a `Main` method is required for the app to compile
+    }
+
+    [UnmanagedCallersOnly]
+    public static int add()
+    {
+        var parameters = Pdk.GetInputJson(global::SourceGenerationContext.Default.Add);
+        var sum = new Sum(parameters.a + parameters.b);
+        Pdk.SetOutputJson(sum, global::SourceGenerationContext.Default.Sum);
+        return 0;
+    }
+}
