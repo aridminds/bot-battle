e51294701f682473458ace07dc3eef685a7a9fd3
Matthias Röhricht <58531609+MRoehricht@users.noreply.github.com>
2024-09-04 23:55:32 +0200

    Missing BulletType serializing

--- src/backend/BotBattle.Tests/AgentRunnerTests.cs
+++ src/backend/BotBattle.Tests/AgentRunnerTests.cs
@@ -1,17 +1,30 @@
 using BotBattle.AgentLib;
+using BotBattle.AgentLib.Enums;
 using BotBattle.AgentRunner;
 using BotBattle.Core;
+using Xunit.Abstractions;
 using Bullet = BotBattle.AgentLib.Bullet;
 using Direction = BotBattle.AgentLib.Enums.Direction;
 using Obstacle = BotBattle.AgentLib.Obstacle;
 using ObstacleType = BotBattle.AgentLib.Enums.ObstacleType;
 using Position = BotBattle.AgentLib.Position;
+using Rotate = BotBattle.AgentLib.Rotate;
+using Shoot = BotBattle.AgentLib.Shoot;
+using Drive = BotBattle.AgentLib.Drive;
 using Tank = BotBattle.AgentLib.Tank;
+using WeaponSystem = BotBattle.AgentLib.WeaponSystem;
 
 namespace BotBattle.Tests;
 
 public class WasmRunnerTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public WasmRunnerTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+    
     [Fact]
     public void TestRun()
     {
@@ -23,7 +36,16 @@ public class WasmRunnerTests
             {
                 Position = new Position { X = 1, Y = 2 },
                 Direction = Direction.North,
-                Health = 100
+                Health = 1000,
+                Name = "player1",
+                WeaponSystem = new WeaponSystem()
+                {
+                    Bullet = BulletType.Standard,
+                    Id = Guid.Empty,
+                    FireCooldown = (float)0.0,
+                    ActiveFireCooldown = (float)0.0,
+                    
+                }
             }
         };
 
@@ -60,7 +82,8 @@ public class WasmRunnerTests
             new()
             {
                 Name = "player1",
-                Code = wasmFile
+              PathToWasm  = @"C:\Users\Matth\source\repos\AridMinds\bot-battle\sample\botbattle_agent_rust\target\wasm32-unknown-unknown\release\botbattle_agent_rust.wasm",
+                
             }
         };
 
@@ -68,5 +91,17 @@ public class WasmRunnerTests
         var response = wasmRunner.Execute("player1", arena);
 
         Assert.NotNull(response);
+
+        if (response.Action is Shoot shoot)
+        {
+            _output.WriteLine($"Shoot: {shoot.Power}");
+        } else if (response.Action is Drive drive)
+        {
+            _output.WriteLine("Drive");
+        }
+        else if(response.Action is Rotate rotate)
+        {
+             _output.WriteLine("Rotate " + rotate.Direction);
+        }
     }
 }
